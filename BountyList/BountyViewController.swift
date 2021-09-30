65e0ba2c90606198f7117b990e74a5b946bc7f81
김경도 <49026505+rudeh567@users.noreply.github.com>
2021-09-30 09:32:46 +0900

    Error Clear

--- BountyList/BountyViewController.swift
+++ BountyList/BountyViewController.swift
@@ -48,13 +48,31 @@ class BountyViewController: UIViewController, UICollectionViewDataSource, UIColl
     
     // UICollectionViewDataSource
     // 몇개를 보여줄까요?
-    // 셀은 어떻게 표현할까요?
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.numOfBountyInfoList
+    }
     
+    // 셀은 어떻게 표현할까요?
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridCell", for: indexPath) as?
+                GridCell else {
+                    return UICollectionViewCell()
+                }
+        
+        let bountyInfo = viewModel.bountyInfo(at: indexPath.item)
+                cell.update(info: bountyInfo)
+        cell.update(info: bountyInfo)
+        return cell
+    }
     
     
     // UICollectionViewDelegate
     // 셀이 클릭되었을때 어쩔까요?
-    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("--> \(indexPath.item)")
+        performSegue(withIdentifier: "showDetail", sender: indexPath.item)
+    }
     
     
     // UICollectionViewDelegateFlowLayout
@@ -97,6 +115,8 @@ class ListCell: UITableViewCell {
     
 }
 
+
+
 class BountyViewModel {
     let bountyInfoList: [BountyInfo] = [
         BountyInfo(name: "brook", bounty: 33000000),
@@ -124,4 +144,17 @@ class BountyViewModel {
     func bountyInfo(at index: Int) -> BountyInfo {
         return sortedList[index]
     }
+}
+
+class GridCell: UICollectionViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLable: UILabel!
+    
+    func update(info: BountyInfo) {
+        imgView.image = info.image
+        nameLabel.text = info.name
+        bountyLable.text = "\(info.bounty)"
+    }
+    
 }
